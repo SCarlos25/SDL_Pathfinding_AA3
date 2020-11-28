e4514c20af7c8f88f49776f276c4cb829587d95d
Joel Mateu <joel.mateu@enti.cat>
2020-11-28 22:51:42 +0100

    ojala el anterior push pero no

--- src/PathFinding.h
+++ src/PathFinding.h
@@ -17,7 +17,10 @@ private:
 	};
 
 	static bool NodeVisited(Node currentNode, const std::vector<std::vector<bool>> &visitedNodes);
-	static float CalculateCostSoFar(const PathData &cameFrom, Node &currentNode, Node &neighborNode);
+	static bool NodeVisited(Node currentNode, const std::vector<std::vector<PathData>> &visitedNodes);
+	static bool NodeValid(Node currentNode, const std::vector<std::vector<PathData>>& visitedNodes, const float &currentCostSoFar);
+	static float CalculateCostSoFar(const float &costSoFar, Node &currentNode, Node &neighborNode);
+	static void DijkstraSort(std::deque<std::pair<Node, float>> &frontier);
 
 public:
 	static std::stack<Node> BFS(Grid *maze, Vector2D start, Vector2D target);
